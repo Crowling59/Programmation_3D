c8b4371789b4c692a2f1a0e692cb4f5e82dcb3d1
Crowling59 <65892014+Crowling59@users.noreply.github.com>
2021-01-20 21:08:50 +0100

    TP1

---
+++ TP1_Desitter_Husson/Assets/SphereExo.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SphereExo : MonoBehaviour
+{
+    //Les [SerializeField] peuvent être modifiés directement depuis unity pendant le fonctionnement de la scene
+    [SerializeField] private GameObject sphereObjet1 = default;
+    [SerializeField] private GameObject sphereObjet2 = default;
+    [SerializeField] private Vector3 centre = default;
+    [SerializeField] private float rayon = 5f;
+    [SerializeField] private int nbPoints = 6;
+    [SerializeField] private float angleRotationX = 90f;
+    [SerializeField] private float angleRotationY = 90f;
+    [SerializeField] private float angleRotationZ = 90f;
+    
+    
+    //DOCUMENTATION ON TRANSFORM OBJECT (Transform _formeCercle, foreach(Transform sousObjet in _formeCercle))
+    //---->https://docs.unity3d.com/ScriptReference/Transform.html
+    private Transform _formeCercle;
+    
+    
+    
+    
+    
+    //DOCUMENTATION ON AWAKE FCT
+    //---->https://docs.unity3d.com/ScriptReference/MonoBehaviour.Awake.html
+    void Awake()
+    {
+        //DOCUMENTATION ON TRANSFORM COMPONENT
+        //---->https://docs.unity3d.com/ScriptReference/Component-transform.html
+        _formeCercle = transform;
+    }
+    void Start()
+    {
+        StartCoroutine(Initialisation());
+        //DOCUMENTATION FOR COROUTINE SETUP (IEnumerator type, StartCoroutine, yield return new WaitForSeconds(arg))
+        //----> https://docs.unity3d.com/ScriptReference/MonoBehaviour.StartCoroutine.html
+    }
+
+    private IEnumerator Initialisation()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            
+            Debug.Log("test");
+            
+            List<Vector3> data = CalculPoints();
+
+            foreach (Transform sousObjet in _formeCercle)
+            {
+                Destroy(sousObjet.gameObject);
+            }
+
+            foreach (Vector3 fablab in data)
+            {
+
+                int i = Random.Range(0, 2);
+                
+                Debug.Log(angleRotationX);
+                Debug.Log(angleRotationY);
+                Debug.Log(angleRotationZ);
+
+                if (i == 0)
+                {
+                    Instantiate(sphereObjet1, fablab, Quaternion.Euler( angleRotationX, angleRotationY,
+                        angleRotationZ), _formeCercle);
+                }
+                else
+                {
+                    Instantiate(sphereObjet2, fablab, Quaternion.Euler( angleRotationX, angleRotationY,
+                        angleRotationZ), _formeCercle);
+                }
+
+            }
+                
+        }
+    }
+
+    private List<Vector3> CalculPoints() {
+
+        List<float> coordonnesX = new List<float> ();
+        List<float> coordonnesY = new List<float> ();
+        List<List<float>> coordonnes = new List<List<float>> ();
+        
+        float angle = 2*Mathf.PI / nbPoints;
+        for (int i = 0; i < nbPoints; i++)
+        {
+            float x = rayon * Mathf.Cos(angle*i);
+            float y = rayon * Mathf.Sin(angle*i);
+            coordonnesX.Add(x);
+            coordonnesY.Add(y);
+            //Debug.Log(coordonnesX[i]);
+            //Debug.Log(coordonnesY[i]);
+        }
+        coordonnes.Add(coordonnesX);
+        coordonnes.Add(coordonnesY);
+        
+        List<Vector3> coordSphere = new List<Vector3>();
+            
+        for (int i = 0; i < coordonnesX.Count; i++) {
+            coordSphere.Add(new Vector3(
+                centre.x + coordonnes[0][i],
+                centre.y + coordonnes[1][i],
+                centre.z));
+            //Debug.Log(coordSphere[i]);
+        }
+        
+        return coordSphere;
+        
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+
+        Quaternion target = Quaternion.Euler(Input.GetAxis("Vertical") * angleRotation, 0,
+            Input.GetAxis("Horizontal") * angleRotation);
+
+        foreach (Transform sousObjet in _formeCercle)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, sousObjet.rotation, Time.deltaTime * 5f);
+        }
+    }*/
+
+
+
+
+    /*if (Input.GetButtonUp("Fire1") && _indexList!=nb_points)
+    {
+        GameObject objectSphere = (GameObject) Instantiate(Resources.Load("Sphere"),new Vector3(x:coordonnes_x[_indexList],y:coordonnes_y[_indexList],z:0f), Quaternion.identity);
+        objects.Add(objectSphere);
+        _indexList++;
+        
+    }
+    
+    if (Input.GetButtonUp("Fire2") && _indexList>0)
+    {
+        Destroy(objects[objects.Count-1]);
+        objects.RemoveAt(objects.Count - 1);
+        _indexList--;
+    }
+    
+    if (Input.GetButtonUp("Jump"))
+    {
+        transform.position=transform.position+ new Vector3(x:0.1f*Input.GetAxis("Horizontal"),y:0.1f*Input.GetAxis("Vertical"), z:0f);
+    }*/
+        
+    
+}
