221961adc68ea795a41d25a1c6d3ffec1c64923b
Crowling59 <65892014+Crowling59@users.noreply.github.com>
2021-01-27 18:15:11 +0100

    wtf_are_my_layers_doing

--- TP2_Desitter_Husson/Assets/Script/CorpsController.cs
+++ TP2_Desitter_Husson/Assets/Script/CorpsController.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class CorpsController : MonoBehaviour
 {
-    private Rigidbody ragdollRigidbody;
+    private Rigidbody _corpsRigidbody;
     [SerializeField] private float speed = 0.0001f;
     
     private void Awake() {
-        ragdollRigidbody = GetComponent<Rigidbody>();
+        _corpsRigidbody = GetComponent<Rigidbody>();
         StartCoroutine(DoMovements());
     }
     
     private IEnumerator DoMovements() {
         while (true) {
-            float timeToDoMovement = Random.Range(2f, 5f);
+            float timeToDoMovement = Random.Range(2f, 6f);
             float timer = 0f;
             Vector3 deltaPosition = new Vector3(Random.Range(-1f, 1f),
-                Random.Range(-0.1f, 0.1f),
+                Random.Range(-0.15f, 0.15f),
                 Random.Range(-1f, 1f)) * speed;
 
             while (timer < timeToDoMovement) {
                 timer += Time.deltaTime;
-                ragdollRigidbody.position += deltaPosition;
+                _corpsRigidbody.position += deltaPosition;
                 yield return new WaitForEndOfFrame();
             }
         }
